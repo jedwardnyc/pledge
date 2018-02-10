264484cf8b84434e86e9347a6b9a829b685c754d
Jedwardnyc <jacobedwardrico@gmail.com>
2018-02-10 12:15:31 -0500

    chaper 1 complete

--- src/pledge.js
+++ src/pledge.js
@@ -4,8 +4,46 @@ Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+function $Promise(executor){
+  if(typeof executor !== 'function'){
+    throw new TypeError('some error with executor and function');
+  }
+  this._state = 'pending';
+  this._handlerGroups = [];
+  executor(this._internalResolve.bind(this),this._internalReject.bind(this))
+}
 
+// $Promise.prototype._state = "pending";
 
+$Promise.prototype._internalResolve = function(input){
+  if(this._state === 'pending'){
+    this._state = 'fulfilled';
+    this._value = input;
+  };
+};
+
+$Promise.prototype._internalReject = function(reason){
+  if(this._state === 'pending'){
+    this._state = 'rejected';
+    this._value = reason;
+  };
+};
+
+$Promise.prototype.then = function (success,error){
+  if (typeof success !== 'function') success = null;
+  if (typeof error !== 'function') error = null;
+  this._handlerGroups.push({successCb: success, errorCb: error})
+  if(this._state !== 'pending'){
+    this._callHandlers();
+  }
+
+}
+
+$Promise.prototype._callHandlers = function(){
+  const val = this._value
+  this._handlerGroups[0].successCb(val)
+  this._handlerGroups.shift();
+}
 
 
 
